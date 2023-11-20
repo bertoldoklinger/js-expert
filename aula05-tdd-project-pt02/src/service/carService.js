3ccb9722d7f20e485f69ed6966630588d356a9ff
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-11-20 00:37:35 -0300

    feat: refactor carService to choose a random available car

--- aula05-tdd-project-pt02/src/service/carService.js
+++ aula05-tdd-project-pt02/src/service/carService.js
@@ -7,9 +7,27 @@ class CarService {
     this.carRepository = new BaseRepository({ file: cars })
   }
 
-  async getAvailableCar(carCategory) {
-    return null
+  getRandomPositionFromArray(list) {
+    const listLength = list.length
+    return Math.floor(
+      Math.random() * (listLength)
+    )
   }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory)
+    const car = await this.carRepository.find(carId)
+
+    return car
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
+    const carId = carCategory.carIds[randomCarIndex]
+
+    return carId
+  }
+
 }
 
 module.exports = CarService
--- aula05-tdd-project-pt02/test/unitTests/carService.test.js
+++ aula05-tdd-project-pt02/test/unitTests/carService.test.js
@@ -2,6 +2,7 @@ const { describe, it } = require('mocha')
 const CarService = require('./../../src/service/carService')
 const { join } = require('path')
 const { expect } = require('chai')
+const sinon = require('sinon')
 
 const carsDatabase = join(__dirname, './../../database', "cars.json")
 
@@ -19,17 +20,59 @@ describe('CarService Suite Tests', () => {
     })
   })
 
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should retrieve a random position from an array', () => {
+    const data = [0, 1, 2, 3, 4]
+    const result = carService.getRandomPositionFromArray(data)
+    //lte é less than equal gte é greater than equal
+    expect(result).to.be.lte(data.length).and.be.gte(0)
+  });
+
+  it('should choose the first id from carsIds in carCategory', () => {
+    const carCategory = mocks.validCarCategory
+    const carIdIndex = 0
+
+    sandbox.stub(
+      carService,
+      carService.getRandomPositionFromArray.name
+    ).returns(carIdIndex)
+
+    const result = carService.chooseRandomCar(carCategory)
+    const expected = carCategory.carIds[carIdIndex]
+
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
+    expect(result).to.be.equal(expected)
+  });
+
   it('given a carCategory it should return an available car ', async () => {
     const car = mocks.validCar
     // o object.create serve para deixar o mock com o mesmo uuid do objeto original, não sujando a instancia
     const carCategory = Object.create(mocks.validCarCategory)
-    carCategory.ids = [car.id]
-    
+    carCategory.carIds = [car.id]
+
+    sandbox.stub(
+      carService.carRepository,
+      carService.carRepository.find.name,
+    ).resolves(car)
+
+    sandbox.spy(
+      carService,
+      carService.chooseRandomCar.name,
+    )
+
     const result = await carService.getAvailableCar(carCategory)
     const expected = car
 
+    expect(carService.chooseRandomCar.calledOnce).to.be.ok
+    expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok
     expect(result).to.be.deep.equal(expected)
   });
 });
-// parei em 14:31 da aula 2
 
